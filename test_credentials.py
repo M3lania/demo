1a57d3a7326d4678b6263d00a52f53cac8f04e60
Melania M <melaniamoldovan@sent.com>
2022-11-22 22:12:06 +0200

    Update test_credentials.py

--- test_credentials.py
+++ test_credentials.py
@@ -1,14 +1,14 @@
-import os
-import pytest
+# import os
+# import pytest
 
-@pytest.fixture(scope='session')
-def my_user():
-    return f"{os.environ['MY_USER']}"
+# @pytest.fixture(scope='session')
+# def my_user():
+#     return f"{os.environ['MY_USER']}"
 
-@pytest.fixture(scope='session')
-def my_password():
-    return f"{os.environ['MY_PASS']}"
+# @pytest.fixture(scope='session')
+# def my_password():
+#     return f"{os.environ['MY_PASS']}"
 
-def test_credentials(my_user, my_password):
-    print(f"\n>>>>> User: {my_user}, \n>>>>> Password: {my_password}")
-    assert "E2E" in my_user and "as" in my_password
+# def test_credentials(my_user, my_password):
+#     print(f"\n>>>>> User: {my_user}, \n>>>>> Password: {my_password}")
+#     assert "E2E" in my_user and "as" in my_password
