c3d37d4757ad6e338c3ba2f90aaa4ee1f32a946b
Melania M <melaniamoldovan@sent.com>
2022-11-22 22:56:10 +0200

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,19 @@
+def pytest_addoption(parser):
+    parser.addoption(
+        "--env",
+        action="store",
+        dest="env",
+        default="staging",
+        choices=("production", "staging"),
+        help="The environment for the application under test."
+    )
+    
+@pytest.fixture(scope="session")
+def env(request):
+    """
+    E.g.: staging, production
+    :param request:
+    :return: The environment for the application under test.
+    """
+    e = request.config.getoption("env")
+    return e.lower()
