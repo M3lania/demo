8d504027c2f37c3c13995ec92af6213118f47386
Melania M <melaniamoldovan@sent.com>
2022-11-24 12:07:14 +0200

    Update test_credentials.py

--- test_credentials.py
+++ test_credentials.py
@@ -1,12 +1,12 @@
-# import os
-# import pytest
+import os
+import pytest
 
 def test_given_environment(env):
     print(env)
 
-# @pytest.fixture(scope='session')
-# def passwords():
-#     return os.environ['PASS_1'], os.environ['PASS_2']
+@pytest.fixture(scope='session')
+def passwords():
+    return os.environ['PASS_1'], os.environ['PASS_2']
 
-# def test_credentials(passwords):
-#     print(passwords)
+def test_credentials(passwords):
+    print(passwords)
